a3d6aba22b95168cb68beba9fa49b4e86ad5e559
Michel San Martin <sanmartin.michels@gmail.com>
2023-03-09 15:29:58 -0300

    refactor del codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,19 @@
 //  H = Hearts, corazon
 //  S = Spades, pica
 
+// Scores y baraja
 let puntosComputador = 0;
 let puntosJugador = 0;
 let baraja = [];
-
+// Referencias del html
+const btnPedirCarta  = document.querySelector( '#btn-pedir-carta' );
+const btnDetener = document.querySelector( '#btn-detener' );
+const btnNuevoJuego = document.querySelector( '#btn-nuevo-juego' );
+const scoreCompu = document.querySelector('#small-c');
+const scoreJugador = document.querySelector('#small-j');
+const jugadorCarta = document.querySelector('#jugador-carta');
+const computadorCarta = document.querySelector('#computador-carta');
+// Simbolos de las cartas
 const tipos = 'CDHS';
 const letras = 'AJQK';
 
@@ -23,7 +32,6 @@ const crearBaraja = ( ) =>{
         for( let alfa of letras){
             baraja.push( alfa + letra )
         }
-
     }
 
     baraja = _.shuffle(baraja);
@@ -46,7 +54,18 @@ const valorCarta = ( carta ) => {
     return ( isNaN( valor ) ) ? (valor === 'A')? 11 : 10: valor * 1;  
 }
 
-// Turno de la computadora
+// MOSTRAR CARTAS
+const mostrarCarta = (carta, mostrarCarta) =>{
+
+    const imgCarta = document.createElement('img');
+
+    imgCarta.className = 'carta';
+    imgCarta.src = `./assets/cartas/${carta}.png`;
+
+    mostrarCarta.append( imgCarta );
+}
+
+// TURNO COMPUTADORA
 const turnoComputadora = ( puntosJugador ) => {
 
     do{
@@ -60,50 +79,7 @@ const turnoComputadora = ( puntosJugador ) => {
     
 }
 
-// Referencias del html
-const btnPedirCarta  = document.querySelector( '#btn-pedir-carta' );
-const btnDetener = document.querySelector( '#btn-detener' );
-const btnNuevoJuego = document.querySelector( '#btn-nuevo-juego' );
-
-const scoreCompu = document.querySelector('#small-c');
-const scoreJugador = document.querySelector('#small-j');
-const jugadorCarta = document.querySelector('#jugador-carta');
-const computadorCarta = document.querySelector('#computador-carta');
-
-
-// CAMBIAR PUNTAJE
-const cambiarPuntaje = (valor, posicion) =>{
-
-    posicion.innerText = valor;
-}
-
-// sirve para activar las funciones del boton DETENER
-const contenidoBoton = () => {
-
-    btnPedirCarta.disabled = true;
-    turnoComputadora( puntosJugador );
-    btnDetener.disabled = true;
-
-    setTimeout(() => {
-        defineGanador( puntosJugador, puntosComputador );    
-    }, 300);
-    
-}
-
-
-
-const mostrarCarta = (carta, mostrarCarta) =>{
-
-    const imgCarta = document.createElement('img');
-
-    imgCarta.className = 'carta';
-    imgCarta.src = `./assets/cartas/${carta}.png`;
-
-    mostrarCarta.append( imgCarta );
-}
-
-
-
+// DEFINE AL GANADOR
 const defineGanador = ( jugador, computador ) => {
 
     if( jugador < computador ){
@@ -123,64 +99,72 @@ const defineGanador = ( jugador, computador ) => {
     else{
         alert('WTFFFF');
     }
-
-     
-
 }
 
+// CAMBIAR PUNTAJE
+const cambiarPuntaje = (valor, posicion) =>{
+    posicion.innerText = valor;
+}
 
-// Eventos
-btnPedirCarta.addEventListener( 'click', () => {
-
+//FUNCIONES BOTONES
+// PEDIR CARTA
+const botonPedirCarta = () => {
+    
     const carta = pedirCarta();
     puntosJugador = puntosJugador + valorCarta(carta);
     mostrarCarta(carta, jugadorCarta);
     cambiarPuntaje( puntosJugador ,scoreJugador );
 
-    // Borrar despues
-    console.log( barajaMezclada );
-    console.log( puntosJugador );
-
     if( puntosJugador > 21 ){
 
         btnPedirCarta.disabled = true;
         console.warn('has perdido');
-        contenidoBoton();
+        botonDetener();
         
-    }else if( puntosJugador == 21){
+    }else if( puntosJugador == 21 ){
         btnPedirCarta.disabled = true;
         console.info('21!, genial');
-        contenidoBoton();
+        botonDetener();
     }
+}
 
-} );
+// DETENER
+const botonDetener = () => {
 
-btnDetener.addEventListener( 'click', () => {
+    btnPedirCarta.disabled = true;
+    turnoComputadora( puntosJugador );
+    btnDetener.disabled = true;
 
-    contenidoBoton();
-    
+    setTimeout( () => defineGanador( puntosJugador, puntosComputador ), 200);
+}
 
-});
+// NUEVO JUEGO
+const botonNuevoJuego = () => {
 
-btnNuevoJuego.addEventListener( 'click', () => {
-
-// Crear la baraja
+    // Crear baraja
     barajaMezclada = [];
     barajaMezclada = crearBaraja();
-// borrar cartas
+    // borrar cartas
     jugadorCarta.innerHTML = '';
     computadorCarta.innerHTML = '';
-// reiniciar marcadores
+    // reiniciar marcadores
     puntosComputador = 0;
     puntosJugador = 0;
     cambiarPuntaje( 0, scoreJugador );
     cambiarPuntaje( 0, scoreCompu );
-    scoreCompu.innerHTML = '';
-    scoreJugador.innerHTML = '';
-// Habilitar botones
+    scoreCompu.innerHTML = '0';
+    scoreJugador.innerHTML = '0';
+    // Habilitar botones
     btnDetener.disabled = false;
     btnPedirCarta.disabled = false;
-    
-});
+}
+
+
+// EVENTOS BOTONES
+btnPedirCarta.addEventListener( 'click', botonPedirCarta );
+
+btnDetener.addEventListener( 'click', () => botonDetener() );
+
+btnNuevoJuego.addEventListener( 'click', () => botonNuevoJuego() );
 
 
